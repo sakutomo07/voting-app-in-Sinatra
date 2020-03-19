9e04d48db87faf05e2266b30a5f30fa72d435cea
sakutomo07 <arai15735320@gmail.com>
2020-03-19 20:29:33 +0900

    Add files
    Add files

---
+++ suffragist.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+
+get '/' do
+  @title = 'Welcome to the Suffragist!'
+  erb :index
+end
+
+post '/cast' do
+  @title = 'Thanks for casting your vote!'
+  @vote  = params['vote']
+  erb :cast
+end
+
+get '/results' do
+  @votes = { 'HAM' => 7, 'PIZ' => 5, 'CUR' => 3 }
+  erb :results
+end
+
+Choices = {
+  'HAM' => 'Hamburger',
+  'PIZ' => 'Pizza',
+  'CUR' => 'Curry',
+  'NOO' => 'Noodles'
+}.freeze
